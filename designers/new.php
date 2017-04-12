93606e25d1b39abe3deb62f0bfe6ba2f95a4bdff
De4aultt <a.zadorozhnyi94@gmail.com>
2017-04-13 01:24:02 +0300

    upgrade DB config

--- designers/new.php
+++ designers/new.php
@@ -2,7 +2,7 @@
 include('../config.php'); 
 if (isset($_POST['submitted'])) { 
 foreach($_POST AS $key => $value) { $_POST[$key] = mysqli_real_escape_string($link, $value); } 
-$sql = "INSERT INTO `Designers` (`Designer_pasport_number` ,`Surname` ,  `Name` ,  `Father_name` ,  `Salary` ,  `Salary` ,  `Email`  ) VALUES( '{$_POST['Designer_pasport_number']}', '{$_POST['Surname']}' ,  '{$_POST['Name']}' ,  '{$_POST['Father_name']}' ,  '{$_POST['Salary']}' ,  '{$_POST['Gender']}' ,  '{$_POST['Email']}'  ) "; 
+$sql = "INSERT INTO `Designers` (`Designer_pasport_number` ,`Surname` ,  `Name` ,  `Father_name` ,  `Salary` ,  `Email` ,  `Email`  ) VALUES( '{$_POST['Designer_pasport_number']}', '{$_POST['Surname']}' ,  '{$_POST['Name']}' ,  '{$_POST['Father_name']}' ,  '{$_POST['Salary']}' ,  '{$_POST['Gender']}' ,  '{$_POST['Email']}'  ) "; 
 mysqli_query($link, $sql) or die(mysqli_error($link)); 
 echo "Added row.<br />"; 
 echo "<a href='list.php'>Back To Listing</a>"; 
@@ -15,5 +15,7 @@ echo "<a href='list.php'>Back To Listing</a>";
 <p><b>Name:</b><br /><textarea name='Name'></textarea> 
 <p><b>Father Name:</b><br /><textarea name='Father_name'></textarea> 
 <p><b>Salary:</b><br /><input type='text' name='Salary'/> 
+<p><b>Gender:</b><br /><textarea name='Gender'></textarea> 
+<p><b>Email:</b><br /><input type='text' name='Email'/> 
 <p><input type='submit' value='Add Row' /><input type='hidden' value='1' name='submitted' /> 
 </form> 
