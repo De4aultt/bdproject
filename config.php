198ba0a8531d7eb8c56a61e41021376b5e45e2f0
De4aultt <a.zadorozhnyi94@gmail.com>
2017-04-13 00:43:12 +0300

    upgrade DB config

--- config.php
+++ config.php
@@ -19,12 +19,12 @@ foreach ($_SERVER as $key => $value) {
     }
     
     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, 'bd_project');
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
