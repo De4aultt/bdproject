992fbf973d978cfdaaf44e84985fb603881d7481
De4aultt <a.zadorozhnyi94@gmail.com>
2017-04-13 14:57:04 +0300

    test

--- includes/connection.php
+++ includes/connection.php
@@ -17,6 +17,7 @@ foreach ($_SERVER as $key => $value) {
 }
 
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
 $link->set_charset("utf8");
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -31,6 +32,5 @@ require("constants.php");
 $con = mysql_connect(DB_SERVER,DB_USER, DB_PASS) or die(mysql_error());
 	mysql_select_db(DB_NAME) or die("Cannot select DB");
 	
-        
 }
 	?>
