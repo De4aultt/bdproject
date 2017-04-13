07a9b8720f27cdbe561146ab4191501938e0c155
De4aultt <a.zadorozhnyi94@gmail.com>
2017-04-13 20:38:18 +0300

    Autorization update

--- acsess_roots.php
+++ acsess_roots.php
@@ -1,4 +1,5 @@
 <?php
+$positions = array("Дизайнер", "Менеджер");
 $designer_pages = array("http://bdproject.azurewebsites.net/index.php",
     "http://bdproject.azurewebsites.net/pictures/delete.php", 
     "http://bdproject.azurewebsites.net/pictures/list.php",
@@ -6,6 +7,30 @@ $designer_pages = array("http://bdproject.azurewebsites.net/index.php",
     "http://bdproject.azurewebsites.net/pictures/new.php",
     );
 
+
+if(isset($_SESSION["session_username"]) && (in_array(strval($_SESSION["session_position"]), $positions))){
+echo "<div id='right'>Вітаю, ";
+echo $_SESSION['session_username']; 
+echo " <a href='../logout.php'>Вийти </a></div>";
+
+function getUrl() {
+  $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+  $url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
+  $url .= $_SERVER["REQUEST_URI"];
+  return $url;
+}    
+ $current_page = getUrl();
+ if (strval($_SESSION["session_position"]) == "Дизайнер" && !(in_array(strval($current_page), $designer_pages))){
+    echo "<h1>Ваша посада '";
+    echo $_SESSION["session_position"];
+    echo "' не має доступу до даної категорії!</h1>";
+    exit();
+}
+}
+else{
+echo "<br> <br><b> Ви не авторизовані.</b> <br> <a href='../login.php'>Вхід</a> <a href='../register.php'>     Реєстрація</a>";
+exit();
+} 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- config.php
+++ config.php
@@ -38,30 +38,6 @@ $link->set_charset("utf8");
 ?>
 
 
-<?php
 
-if(isset($_SESSION["session_username"])){
-echo "<div id='right'>Вітаю, ";
-echo $_SESSION['session_username']; 
-echo " <a href='../logout.php'>Вийти </a></div>";
-
-function getUrl() {
-  $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
-  $url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
-  $url .= $_SERVER["REQUEST_URI"];
-  return $url;
-}    
- $current_page = getUrl();
- if (strval($_SESSION["session_position"]) == "Дизайнер" && !(in_array(strval($current_page), $designer_pages))){
-    echo "<h1>Ваша посада '";
-    echo $_SESSION["session_position"];
-    echo "' не має доступу до даної категорії!</h1>";
-    exit();
-}
-}
-else{
-echo "<br> <br><b> Ви не авторизовані.</b> <br> <a href='../login.php'>Вхід</a> <a href='../register.php'>     Реєстрація</a>";
-exit();
-} 
 
 
